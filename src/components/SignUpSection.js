b0b9b2710b63364dd920ef4297037990f53ccf9d
David Gamero <david340804@gmail.com>
2019-12-19 15:14:23 -0500

    Sign up button

--- src/components/SignUpSection.js
+++ src/components/SignUpSection.js
@@ -2,13 +2,23 @@ import React from "react"
 import { StaticQuery, graphql } from "gatsby"
 import styled from 'styled-components'
 
+import Button from 'react-bootstrap/Button';
+
 import BackgroundImage from 'gatsby-background-image'
+
+const SignUpButton = styled(Button)`
+  font-size: 1rem;
+
+  @media(min-width: 750px) {
+    font-size: 2rem;
+  }
+`
 
 const Div = styled.div`
   text-align: left;
   padding: 1rem;
 
-  @media(min-width: 750px){
+  @media(min-width: 750px) {
     padding: 4rem;
   }
 `
@@ -20,36 +30,47 @@ const H1 = styled.h1`
   font-size: 2rem;
   font-weight: bold;
 
-  @media(min-width: 750px){
-    font-size: 3rem;
+  @media(min-width: 750px) {
+    font-size: 2rem;
   }
 `
 
-const P = styled.p`
+const H3 = styled.h1`
   color: white;
   margin: 0;
   font-family: "Helvetica Neue";
   font-size: 1rem;
-  line-height: 4rem;
 
-  @media(min-width: 750px){
-    font-size: 2rem;
+  @media(min-width: 750px) {
+    font-size: 1rem;
   }
+`
+
+const P = styled.p`
+color: white;
+margin: 0;
+font-family: "Helvetica Neue";
+font-size: 1rem;
+line-height: 4rem;
+
+@media(min-width: 750px) {
+  font-size: 1rem;
+}
 `
 
 const SignUpSection = ({ className }) => (
   <StaticQuery
     query={graphql`
-query {
-  desktop: file(relativePath: { eq: "sign-up-bike-bg.jpg" }) {
-    childImageSharp {
-      fluid(quality: 100, maxWidth: 1920) {
-              ...GatsbyImageSharpFluid_withWebp
+      query {
+        desktop: file(relativePath: { eq: "sign-up-bike-bg.jpg" }) {
+          childImageSharp {
+            fluid(quality: 100, maxWidth: 1920) {
+                    ...GatsbyImageSharpFluid_withWebp
+            }
+          }
+        }
       }
-    }
-  }
-}
-`}
+    `}
     render={data => {
       // Set ImageData.
       const imageData = data.desktop.childImageSharp.fluid
@@ -61,11 +82,20 @@ query {
           backgroundColor={`#040e18`}
         >
           <Div>
+            {/* #TODO Break into two lines on mobile */}
             <H1>Burn Rubber, Not Gas.</H1>
-            <H1>No Contract.</H1>
-            <H1>No BS.</H1>
-            <H1>Get Where Your Going.</H1>
-            <P>Sign up for our beta launching in two weeks</P>
+
+            {/* <H1>No Contract. No BS.</H1>
+            <H1>Get Where Your Going.</H1> */}
+
+            <H3>Introducing Skrrt, the first ever electric cafe racer subscription</H3>
+            <P>Our beta is launching in Atlanta in two weeks</P>
+            {/* #TODO Get early access to the beta in two weeks */}
+            {/* #TODO Bold, small, move to top left*/}
+
+            <SignUpButton variant="outline-light">
+              Sign up now
+            </SignUpButton>
 
           </Div>
 
@@ -80,7 +110,7 @@ const StyledSignUpSection = styled(SignUpSection)`
   height: 100vh;
   margin: 0;
   background-position: left;
-  background-repeat: repeat-y;
+  background-repeat: repeat - y;
   background-size: cover;
   display: flex;
   justify-content: left;
